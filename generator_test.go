d0590274a393e9e3f8aa5ccac8d19b1bfaa1b17e
Takafumi SEKIGUCHI <takkyuuplayer@gmail.com>
2022-07-13 12:50:10 +0900

    fix: model generation may fail on MySQL8 (#30)

--- generator_test.go
+++ generator_test.go
@@ -11,59 +11,65 @@ import (
 )
 
 func TestGenerator_Generate(t *testing.T) {
-	db := testDb()
-	g := NewGenerator(db.DB())
-	checkFiles := func(dir string, elements []string) {
-		entries, err := os.ReadDir(dir)
-		assert.Nil(t, err)
-		var files []string
-		for _, e := range entries {
-			files = append(files, e.Name())
-		}
-		assert.ElementsMatch(t, files, elements)
+	for version, db := range map[string]DB{
+		"mysql5.7": testDb(),
+		"mysql8":   testDbMySQL8(),
+	} {
+		t.Run(version, func(t *testing.T) {
+			g := NewGenerator(db.DB())
+			checkFiles := func(dir string, elements []string) {
+				entries, err := os.ReadDir(dir)
+				assert.Nil(t, err)
+				var files []string
+				for _, e := range entries {
+					files = append(files, e.Name())
+				}
+				assert.ElementsMatch(t, files, elements)
+			}
+			t.Run("basic", func(t *testing.T) {
+				dir, err := os.MkdirTemp(os.TempDir(), "dist")
+				assert.Nil(t, err)
+				err = g.Generate(&GenerateOptions{
+					OutDir:  dir,
+					Package: "dist",
+				})
+				if err != nil {
+					log.Errorf(err.Error())
+				}
+				assert.Nil(t, err)
+				checkFiles(dir, []string{"users.go", "user_groups.go", "user_login_histories.go", "group_users.go", "fields.go"})
+			})
+			t.Run("exclude", func(t *testing.T) {
+				dir, _ := os.MkdirTemp(os.TempDir(), "dist")
+				err := g.Generate(&GenerateOptions{
+					OutDir:  dir,
+					Package: "dist",
+					Exclude: []string{"fields"},
+				})
+				if err != nil {
+					log.Errorf(err.Error())
+				}
+				assert.Nil(t, err)
+				checkFiles(dir, []string{"users.go", "user_groups.go", "user_login_histories.go", "group_users.go"})
+			})
+
+			t.Run("should return error when rows.Error() return error", func(t *testing.T) {
+				mockDb, mock, err := sqlmock.New()
+				assert.NoError(t, err)
+				defer mockDb.Close()
+
+				mock.ExpectQuery(`show tables`).WillReturnRows(
+					sqlmock.NewRows([]string{"tables"}).
+						AddRow("users").
+						RowError(0, fmt.Errorf("err")))
+
+				dir, err := os.MkdirTemp(os.TempDir(), "dist")
+				assert.NoError(t, err)
+				assert.EqualError(t, NewGenerator(mockDb).Generate(&GenerateOptions{
+					OutDir:  dir,
+					Package: "dist",
+				}), "err")
+			})
+		})
 	}
-	t.Run("basic", func(t *testing.T) {
-		dir, err := os.MkdirTemp(os.TempDir(), "dist")
-		assert.Nil(t, err)
-		err = g.Generate(&GenerateOptions{
-			OutDir:  dir,
-			Package: "dist",
-		})
-		if err != nil {
-			log.Errorf(err.Error())
-		}
-		assert.Nil(t, err)
-		checkFiles(dir, []string{"users.go", "user_groups.go", "user_login_histories.go", "group_users.go", "fields.go"})
-	})
-	t.Run("exclude", func(t *testing.T) {
-		dir, _ := os.MkdirTemp(os.TempDir(), "dist")
-		err := g.Generate(&GenerateOptions{
-			OutDir:  dir,
-			Package: "dist",
-			Exclude: []string{"fields"},
-		})
-		if err != nil {
-			log.Errorf(err.Error())
-		}
-		assert.Nil(t, err)
-		checkFiles(dir, []string{"users.go", "user_groups.go", "user_login_histories.go", "group_users.go"})
-	})
-
-	t.Run("should return error when rows.Error() return error", func(t *testing.T) {
-		mockDb, mock, err := sqlmock.New()
-		assert.NoError(t, err)
-		defer mockDb.Close()
-
-		mock.ExpectQuery(`show tables`).WillReturnRows(
-			sqlmock.NewRows([]string{"tables"}).
-				AddRow("users").
-				RowError(0, fmt.Errorf("err")))
-
-		dir, err := os.MkdirTemp(os.TempDir(), "dist")
-		assert.NoError(t, err)
-		assert.EqualError(t, NewGenerator(mockDb).Generate(&GenerateOptions{
-			OutDir:  dir,
-			Package: "dist",
-		}), "err")
-	})
 }
--- test_db.go
+++ test_db.go
@@ -13,6 +13,16 @@ func testDb() DB {
 	return db
 }
 
+func testDbMySQL8() DB {
+	db, err := Open(&OpenOptions{
+		Url: "root:@tcp(127.0.0.1:3327)/exql?charset=utf8mb4&parseTime=True&loc=Local",
+	})
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
+
 func testSqlDB() *sql.DB {
 	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3326)/exql?charset=utf8mb4&parseTime=True&loc=Local")
 	if err != nil {
